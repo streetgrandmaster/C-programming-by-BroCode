dd2e7dec6f2bacf4e79362232c62b65f5feaba65
LAWRENCE <laurielawi12@gmail.com>
2022-04-21 08:47:31 +0300

    math functions

---
+++ 8-math-functions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    // math functions
+
+    double A = sqrt(121);
+    double B = pow(4, 3);
+    int C = round(3.14159);
+    int D = ceil(3.14159);
+    int E = floor(3.14159);
+    double F = fabs(-100);
+    double G = log(10);
+    double H = sin(90);
+    double I = cos(90);
+    double J = tan(90);
+
+    printf("%lf\n", A);
+    printf("%lf\n", B);
+    printf("%d\n", C);
+    printf("%d\n", D);
+    printf("%d\n", E);
+    printf("%lf\n", F);
+    printf("%lf\n", G);
+    printf("%lf\n", H);
+    printf("%lf\n", I);
+    printf("%lf\n", J);
+
+
+    return 0;
+}
