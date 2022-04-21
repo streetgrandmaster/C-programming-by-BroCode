a874cf2645c7cf5f8b2113e6a3222dfb56f8d430
LAWRENCE <laurielawi12@gmail.com>
2022-04-21 08:45:22 +0300

    arithmetic operators

---
+++ 5-arithmetic-operators.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    // arithmetic operators
+
+    // + addition
+    // - subraction
+    // * multiplication
+    // / division
+    // % modulus
+    // ++ increment
+    // -- decrement
+
+    // int num1 = 77;
+    // int num2 = 12;
+    // int operators_1 = num1 * num2;
+    // printf("%d\n", operators_1);
+
+    // // division
+    // int num1 = 77;
+    // int num2 = 12;
+    // float operators_1 = num1 / (float)num2;
+    // printf("%f\n", operators_1);
+
+    // // modulus
+    // int num1 = 77;
+    // int num2 = 12;
+    // int operators_1 = num1 % num2;
+    // printf("%d\n", operators_1);
+
+    // // increment & decrement
+    // int num1 = 77;
+    // int num2 = 12;
+    // num1++;
+    // num2--;
+    // printf("%d\n", num1);
+    // printf("%d\n", num2);
+    
+
+    return 0;
+}
