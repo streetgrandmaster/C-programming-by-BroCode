22f365ca319f435f30fb1d2442b19efc15b1a466
LAWRENCE <laurielawi12@gmail.com>
2022-04-21 08:46:13 +0300

    augmented assignment operators

---
+++ 6-augmented-assign-oper.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    // augmented assignment operators = Used to replace a statement
+    //          where an operator takes a variable as one of its arguments
+    //          and then assigns the result back to the same variable.
+    //
+        //          my_num = 10
+        //          my_num = my_num + 1
+        //          my_num +=1
+
+    int my_num = 100;
+    // // my_num = my_num + 34;
+    // my_num+=34;
+
+    // // my_num = my_num - 34;
+    // my_num-=34;
+
+    // // my_num = my_num * 34;
+    // my_num*=34;
+
+    // // my_num = my_num / 34;
+    // my_num/=34;
+
+    // // my_num = my_num % 34;
+    // my_num%=34;
+
+    printf("%d\n", my_num);
+
+    return 0;
+}
