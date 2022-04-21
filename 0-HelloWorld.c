39021c8f072bfdadc420dde0f04582596f1e70b5
LAWRENCE <laurielawi12@gmail.com>
2022-04-21 08:38:19 +0300

    Hello world, comments & escape sequences

---
+++ 0-HelloWorld.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    // This is my first single line comment
+
+    /**
+     * This is
+     * multiline
+     * comment
+     * 
+     * 
+     * Escape sequences = Character combination consisting of a backslash(\)
+     *                    followed by a letter or combination of digits.
+     *      \n = New line
+     *      \t = Tab
+     */
+    
+    printf("I love C \n");
+    printf("C is middle level programming language.\n");
+
+
+    // Using \n and \t
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+    // Printing quotes
+    printf("\"Eductation is the equalizer between the rich and the poor.\" - By Anonymous.");
+
+    return 0;
+}
