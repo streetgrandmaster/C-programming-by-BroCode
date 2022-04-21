ae9145d35127f39844494aebeed6bad9c5932a57
LAWRENCE <laurielawi12@gmail.com>
2022-04-21 08:54:55 +0300

    16-functions

---
+++ 16-functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+void birthday()
+{
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy dear Lawi!");
+    printf("\nHappy birthday to you!\n");
+}
+
+int main(){
+
+    // functions
+    birthday();
+    birthday();
+
+    return 0;
+}
